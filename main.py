55d70013ff354735b303198469c98df133ab1295
LauraOrtiz-235 <71852276+LauraOrtiz-235@users.noreply.github.com>
2020-11-25 13:43:41 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,30 +5,30 @@ SCREEN_WIDTH = 640
 SCREEN_HEIGHT = 480
 
 def main():
-    # Inicialización de todos los módulos importados de pygame
+    # Inicialización de todos los módulos importados de pygame y que se usaron
     pygame.init()
-    # Se pone el ancho y el altura de la pantalla [ancho,altura]
+    # Se pone el ancho y el altura de la pantalla 
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
     # Establecer el título de la ventana emergente
-    pygame.display.set_caption("Macctematicas")
-    # Bucle hasta que el usiario oprima el boton de cerrar la ventana
+    pygame.display.set_caption("MACCtemáticas")
+    # Bucle hasta que el usuario oprima el boton de cerrar la ventana
     done = False
     # Se usa para manejar que tan rápido se actializa la pantalla
     clock = pygame.time.Clock()
     # Se crea el objeto game
     game = Game()
-    # -------- Bucle Principal del Programa -----------
+    # Bucle Principal del Programa 
     while not done:
-        # --- Se procesan los eventos (pulsaciones de tecla, clicks,etc)
+        # Se procesan los eventos (pulsaciones de tecla, clicks,etc)
         done = game.process_events()
-        # --- Lógica del juego
+        # Lógica del juego
         game.run_logic()
-        # --- Se dibuja los recuadros de la pantalla
+        # Se dibuja los recuadros de la pantalla
         game.display_frame(screen)
-        # --- Se limita a 30 recuadros por segundo
+        # Se limita a 30 recuadros por segundo
         clock.tick(30)
 
-    # Se cierra la ventana y se quit
+    # Se cierra la ventana
     pygame.quit()
 
 if __name__ == '__main__':
